dbc6453418b10e63e6dfa488708947afe8765d21
Felipe Mignone <dev.felipemignone@gmail.com>
2026-02-20 11:09:44 -0300

    feat: start project

---
+++ src/main/java/com/api/marketplaceconsumer/MarketplaceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.api.marketplaceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceConsumerApplication.class, args);
+    }
+
+}
